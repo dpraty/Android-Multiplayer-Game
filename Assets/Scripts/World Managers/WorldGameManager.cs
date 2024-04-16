a22333a862c49c1269414554c4dc2acb70102fe7
PiyushDivyankar <piyush0divyankar@gmail.com>
2024-04-17 02:39:06 +0530

    Added Unity Relay; Tested Online Multiplayer on Phone

--- Assets/Scripts/World Managers/WorldGameManager.cs
+++ Assets/Scripts/World Managers/WorldGameManager.cs
@@ -9,19 +9,18 @@ public class WorldGameManager : MonoBehaviour
     // base world game manager class - singleton
 
     public static WorldGameManager instance;
+    public TestRelay relay;
 
     // Scene List - currently only has the world scene
     [Header("Scene Index")]
     [SerializeField] int worldSceneIndex = 1;
-
-    [Header("Network Join")]
-    [SerializeField] bool startGameAsClient;
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            relay = GetComponent<TestRelay>();
         }
         else
         {
@@ -36,14 +35,10 @@ public class WorldGameManager : MonoBehaviour
 
     private void Update()
     {
-        StartGameAsClient();
+
     }
 
     // start host network
-    public void StartNetworkAsHost()
-    {
-        NetworkManager.Singleton.StartHost();
-    }
 
     // an Async/Corutine thing to load scenes??
     public IEnumerator LoadWorldScene()
@@ -57,19 +52,15 @@ public class WorldGameManager : MonoBehaviour
     // it starts the host network and loads the game scene
     public void StartNewGame()
     {
-        StartNetworkAsHost();
+        NetworkManager.Singleton.StartHost();
         StartCoroutine(WorldGameManager.instance.LoadWorldScene());
     }
 
     // to start game as client first shutdown the host network then start a client
-    public void StartGameAsClient()
+    public void StartGameAsClient(string joinCode)
     {
-        if (startGameAsClient)
-        {
-            startGameAsClient = false;
-            NetworkManager.Singleton.Shutdown();
-            NetworkManager.Singleton.StartClient();
-        }
+        relay.JoinRelay(joinCode);
+        StartCoroutine(WorldGameManager.instance.LoadWorldScene());
     }
 
     // this get world scene index function is for utility - currently used for a clever UI switiching logic
